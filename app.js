f63f26485c1f450c55b1877a96bd7afef2557f79
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-20 15:01:20 +0500

    app listens on port 5000

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json({ extended: true }));
+
+app.listen(5000, function () {
+  console.log("Server is running on port 5000...");
+});
