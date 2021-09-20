750e2bf44eb493dbb881e43ae4962a4088a6153f
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-20 16:51:17 +0500

    require request and add get

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 
-app.listen(5000, function () {
-  console.log("Server is running on port 5000...");
+app.get("/", (req, res) => {
+  res.send("Yay. It's working...");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000...");
 });
