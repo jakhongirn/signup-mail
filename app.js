4f69583f1c076d02542f5e8b1fc907e9a347e63a
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-20 18:24:43 +0500

    use public folder for static files

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const request = require("request");
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/signup.html")
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.listen(3000, function () {
