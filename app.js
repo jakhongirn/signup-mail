9ac4cc441f071c6c841d5703776e30ace6d2641f
Jakhongir <jahongirnagmatov@gmail.com>
2021-09-20 19:05:51 +0500

    add post and fixed bug in line 8: change json to urlencoded

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
-app.use(bodyParser.json({ extended: true }));
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const secondName = req.body.secondName;
+    const email = req.body.email;
+
+    console.log(firstName, secondName, email);
+})
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000...");
